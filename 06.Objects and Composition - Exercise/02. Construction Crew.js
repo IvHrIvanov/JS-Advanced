ab04957b13b1fcd9ed63d02b79ec8187cefaa3a4
IvHrIvanov <hristoslavov.ivanov@gmail.com>
2021-06-03 20:49:30 +0300

    Callorie,Construction Crew,Car Factory

---
+++ 06.Objects and Composition - Exercise/02. Construction Crew.js
@@ -0,0 +1,33 @@
+function solve(worker) {
+
+    if (worker.dizziness == true) {
+        worker.levelOfHydrated += (worker.weight * 0.1) * worker.experience;
+        worker.dizziness = false;
+    }
+    return worker;
+}
+let worker =
+{
+    weight: 80,
+    experience: 1,
+    levelOfHydrated: 0,
+    dizziness: true
+};
+console.log(solve(worker)); //{weight: 80, experience: 1, levelOfHydrated: 8, dizziness: false}
+worker =
+{
+    weight: 120,
+    experience: 20,
+    levelOfHydrated: 200,
+    dizziness: true
+};
+console.log(solve(worker)); //{weight: 120, experience: 20, levelOfHydrated: 440, dizziness: false}
+worker =
+{
+    weight: 95,
+    experience: 3,
+    levelOfHydrated: 0,
+    dizziness: false
+}
+
+console.log(solve(worker)); //{weight: 95, experience: 3, levelOfHydrated: 0, dizziness: false}
