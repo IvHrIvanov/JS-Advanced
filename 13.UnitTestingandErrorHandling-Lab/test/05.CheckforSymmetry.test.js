a9d3dbe9b235c0bfdd3a46ff14927bd599aa4e31
IvHrIvanov <hristoslavov.ivanov@gmail.com>
2021-06-21 02:29:52 +0300

    Unit Test Exercises

---
+++ 13.UnitTestingandErrorHandling-Lab/test/05.CheckforSymmetry.test.js
@@ -0,0 +1,58 @@
+// const isSymmetric = require('../05.CheckforSymmetry');
+// const { assert } = require('chai');
+
+// describe('TestArray Return True/False, Input', () => {
+//     it('Should be symmetry RETURN TRUE', () => {
+//         let numbers = [1, 2, 3, 2, 1];
+//         let espectedResult = true;
+//         let actualResult = isSymmetric(numbers);
+//         assert.equal(actualResult, espectedResult);
+
+//     });
+//     it('Should be symmetry RETURN TRUE', () => {
+//         let numbers = [1, 2, 3, 3, 1];
+//         let espectedResult = false;
+//         let actualResult = isSymmetric(numbers);
+//         assert.equal(actualResult, espectedResult);
+
+//     });
+//     it('Should be other input', () => {
+    
+//         let espectedResult = false;
+//         assert.equal(isSymmetric(null), espectedResult);
+//         assert.equal(isSymmetric(NaN), espectedResult);
+//         assert.equal(isSymmetric(undefined), espectedResult);      
+//         assert.equal(isSymmetric({}), espectedResult);
+//         assert.equal(isSymmetric(0), espectedResult);
+//         assert.equal(isSymmetric(''), espectedResult);
+       
+       
+
+//     });
+ 
+//     it('Should be TRUE', () => {
+//         let numbers = [];
+//         let espectedResult = true;
+//         let actualResult = isSymmetric(numbers);
+//         assert.equal(actualResult, espectedResult);
+
+//     });
+//     it('Should be 1', () => {
+//         let numbers = ['1',1];
+//         let espectedResult = false;
+//         let actualResult = isSymmetric(numbers);
+//         assert.equal(actualResult, espectedResult);
+
+//     });
+// });
+
+
+
+
+
+
+
+// // •	Take an array as argument
+// // •	Return false for any input that isn’t of the correct type
+// // •	Return true if the input array is symmetric
+// // •	Otherwise, return false
